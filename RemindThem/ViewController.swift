06322ddc4092126af59c998dc808d00c6064a016
Dallin <dallinmcconnell@gmail.com>
2018-07-30 13:43:42 -0600

    Initial Commit

---
+++ RemindThem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemindThem
+//
+//  Created by Dallin McConnell on 7/30/18.
+//  Copyright © 2018 Dallin McConnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
